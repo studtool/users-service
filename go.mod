58300d8c94ceeab2fdad71d393604b8f2370524e
denis96z <zinoviev.denis@yandex.ru>
2019-04-07 17:24:28 +0300

    main() implementation

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/mux v1.7.1
 	github.com/studtool/common v0.0.0-20190406204519-a197e7fd43a3
+	go.uber.org/dig v1.7.1-0.20190327003609-4cc9f8898fe3
 )
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
 
+	"go.uber.org/dig"
+
+	"github.com/studtool/users-service/api"
+	"github.com/studtool/users-service/beans"
+	"github.com/studtool/users-service/config"
+)
+
+func main() {
+	c := dig.New()
+
+	panicOnErr(c.Provide(api.NewServer))
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt)
+
+	_ = c.Invoke(func(srv *api.Server) {
+		go func() {
+			if err := srv.Run(); err != nil {
+				beans.Logger.Fatal(err)
+				ch <- os.Interrupt
+			}
+		}()
+
+		beans.Logger.Info(fmt.Sprintf("server: started; [port: %s]", config.ServerPort.Value()))
+	})
+	defer func() {
+		_ = c.Invoke(func(srv *api.Server) {
+			if err := srv.Shutdown(); err != nil {
+				beans.Logger.Fatal(err)
+			} else {
+				beans.Logger.Info("server: down")
+			}
+		})
+	}()
+
+	<-ch
+}
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
