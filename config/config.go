8df09cbfa1077ba41e3197091dadaa0f70ff6330
denis96z <zinoviev.denis@yandex.ru>
2019-04-13 20:19:16 +0300

    refactoring

--- config/config.go
+++ config/config.go
@@ -4,18 +4,12 @@ import (
 	"github.com/studtool/common/config"
 )
 
-const (
-	ServiceName = "auth-service"
-)
-
 var (
 	ServerPort = config.NewStringDefault("STUDTOOL_USERS_SERVICE_PORT", "80")
 
 	RepositoriesEnabled = config.NewFlagDefault("STUDTOOL_USERS_SERVICE_REPOSITORIES_ENABLED", false)
 
-	StorageHost     = config.NewStringDefault("STUDTOOL_USERS_STORAGE_HOST", "127.0.0.1")
-	StoragePort     = config.NewStringDefault("STUDTOOL_USERS_STORAGE_PORT", "27017")
-	StorageDB       = config.NewStringDefault("STUDTOOL_USERS_STORAGE_NAME", "users")
-	StorageUser     = config.NewStringDefault("STUDTOOL_USERS_STORAGE_USER", "user")
-	StoragePassword = config.NewStringDefault("STUDTOOL_USERS_STORAGE_PASSWORD", "password")
+	StorageHost = config.NewStringDefault("STUDTOOL_USERS_STORAGE_HOST", "127.0.0.1")
+	StoragePort = config.NewStringDefault("STUDTOOL_USERS_STORAGE_PORT", "27017")
+	StorageDB   = config.NewStringDefault("STUDTOOL_USERS_STORAGE_NAME", "users")
 )
